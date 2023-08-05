abde59d37468c771500b3585dc5861280faf0928
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-08-05 10:59:51 +0200

    Set up layout

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,23 @@
 use leptos::*;
 use leptos_meta::*;
-use leptos_router::*;
 
+use crate::model::conversation::{Conversation, Message};
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
+
+    let (conversation, set_conversation) = create_signal(cx, Conversation::new());
+
+    let send = create_action(cx, move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversation.update(move |c| {
+            c.messages.push(user_message);
+        });
+    });
 
     view! { cx,
         // injects a stylesheet into the document <head>
@@ -14,7 +26,7 @@ pub fn App(cx: Scope) -> impl IntoView {
 
         // sets the document title
         <Title text="LLaMA ChatBot"/>
-        <ChatArea/>
-        <TypeArea/>
+        <ChatArea conversation/>
+        <TypeArea send/>
     }
 }
