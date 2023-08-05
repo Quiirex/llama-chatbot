0ab80f89959109f3c817494407b1eadd3b3cc3e6
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-08-05 12:02:31 +0200

    Add sending and receiving effects

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,15 @@
 use leptos::*;
 use leptos_meta::*;
 
+mod components;
+use components::chat_area::ChatArea;
+use components::type_area::TypeArea;
+
+use crate::api::converse;
 use crate::model::conversation::{Conversation, Message};
+
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
 
     let (conversation, set_conversation) = create_signal(cx, Conversation::new());
@@ -17,15 +22,34 @@ pub fn App(cx: Scope) -> impl IntoView {
         set_conversation.update(move |c| {
             c.messages.push(user_message);
         });
+        converse(cx, conversation.get())
+    });
+
+    create_effect(cx, move |_| {
+        if let Some(_) = send.input().get() {
+            let model_message = Message {
+                text: String::from("..."),
+                user: false,
+            };
+
+            set_conversation.update(move |c| {
+                c.messages.push(model_message);
+            });
+        }
+    });
+
+    create_effect(cx, move |_| {
+        if let Some(Ok(response)) = send.value().get() {
+            set_conversation.update(move |c| {
+                c.messages.last_mut().unwrap().text = response;
+            });
+        }
     });
 
     view! { cx,
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
 
-        // sets the document title
-        <Title text="LLaMA ChatBot"/>
+        <Title text="Llama Chatbot"/>
         <ChatArea conversation/>
         <TypeArea send/>
     }
